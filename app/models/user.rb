7a03d0ed0c0820e6f128189c8156c7243d52c1bf
Ken Ohata <ohata@daraf.co.jp>
2014-12-27 02:04:46 +0900

    validate User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { in: 6..18 }, confirmation: true, if: :password
+  validates :password_confirmation, presence: true, if: :password
 end
