daddca9df967fb4f93d061f6debf23104ff89ec7
Ken Ohata <ohata@daraf.co.jp>
2014-12-27 02:13:03 +0900

    implement sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,28 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    email    = params_session[:email]
+    password = params_session[:password]
+
+    if @user = login(email, password)
+      redirect_to new_sessions_path
+    else
+      @user = User.new
+      redirect_to new_sessions_path
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to new_sessions_path
+  end
+
+  private
+
+  def params_session
+    params.require(:user).permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+
+  resource :sessions, only: [:new, :create, :destroy]
+
+  root to: 'sessions#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
